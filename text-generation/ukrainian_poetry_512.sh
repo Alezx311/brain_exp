ebe84c60697a9f461738d99375f1264bdd44d1fd
Oleksii Sokolov <alezx311@gmail.com>
2022-09-23 20:36:24 +0300

    UPD | Add some generated data

--- text-generation/ukrainian_poetry_512.sh
+++ text-generation/ukrainian_poetry_512.sh
@@ -2,7 +2,7 @@
 
 PATH_INPUT=./ukrainian_poetry.txt
 PATH_OUTPUT_DIR=./ukrainian_poetry_512/
-PATH_OUTPUT_GENERATED=./ukrainian_poetry_512/generated.txt
+PATH_OUTPUT_GENERATED=./ukrainian_poetry_512/ukrainian_poetry_512_generated.txt
 PATH_MODEL=./ukrainian_poetry_512/model.json
 GEN_SIZE=1000
 GEN_SIZE_LARGE=10000
@@ -15,7 +15,7 @@ echo "GEN_SIZE" $GEN_SIZE
 echo "GEN_SIZE_LARGE" $GEN_SIZE_LARGE
 
 echo "Training network"
-yarn train $PATH_INPUT --lstmLayerSize 512,512 --epochs 300 --savePath $PATH_OUTPUT_DIR --gpu
+yarn train $PATH_INPUT --lstmLayerSize 512,512 --epochs 100 --savePath $PATH_OUTPUT_DIR --gpu
 echo "Training network success!"
 
 echo "Generating data with default temperature" >> $PATH_OUTPUT_GENERATED
@@ -31,5 +31,8 @@ echo "Generating data with temperature 0.75" >> $PATH_OUTPUT_GENERATED
 yarn gen $PATH_INPUT $PATH_MODEL --genLength $GEN_SIZE --temperature 0.75 >> $PATH_OUTPUT_GENERATED 
 echo "Generating data with temperature 0.9" >> $PATH_OUTPUT_GENERATED
 yarn gen $PATH_INPUT $PATH_MODEL --genLength $GEN_SIZE --temperature 0.9 >> $PATH_OUTPUT_GENERATED 
+
+echo "Generating data with default temperature" >> $PATH_OUTPUT_GENERATED
+yarn gen $PATH_INPUT $PATH_MODEL --genLength $GEN_SIZE_LARGE >> $PATH_OUTPUT_GENERATED
 
 echo "Generating data success!"
